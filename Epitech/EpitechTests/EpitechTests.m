c93a64c73d897627befa8159b57995d0b1364441
rémi <remirobert33530@gmail.com>
2015-01-15 18:40:00 +0100

    add Test connection network

--- Epitech/EpitechTests/EpitechTests.m
+++ Epitech/EpitechTests/EpitechTests.m
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+#import "NetworkRequest.h"
 
 @interface EpitechTests : XCTestCase
 
@@ -23,6 +24,70 @@
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
+}
+
+- (void) testLoginConnectionGood {
+
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Connection Good"];
+    NSDictionary *params = @{@"login":@"robert_r", @"password":@"fl5>[dWn"};
+
+    [NetworkRequest POST:@"https://epitech-api.herokuapp.com/login" parameters:params blockCompletion:^(AFHTTPRequestOperation *operation, id responseObject) {
+        [expectation fulfill];
+        NSLog(@"%@", responseObject);
+        XCTAssert(responseObject, @"Pass");
+    } andErrorCompletion:^(AFHTTPRequestOperation *operation, NSError *error) {
+        [expectation fulfill];
+        XCTFail(@"fail");
+    }];
+    
+    [self waitForExpectationsWithTimeout:5 handler:nil];
+}
+
+- (void) testLoginConnectionWrongUsername {
+    
+    XCTestExpectation *expectation = [self expectationWithDescription:@"connection with wrong username"];
+    NSDictionary *params = @{@"login":@"robert_fdsr", @"password":@"fl5>[dWn"};
+    
+    [NetworkRequest POST:@"https://epitech-api.herokuapp.com/login" parameters:params blockCompletion:^(AFHTTPRequestOperation *operation, id responseObject) {
+        [expectation fulfill];
+        XCTFail(@"fail");
+    } andErrorCompletion:^(AFHTTPRequestOperation *operation, NSError *error) {
+        [expectation fulfill];
+        XCTAssert(true, @"Pass");
+    }];
+    
+    [self waitForExpectationsWithTimeout:5 handler:nil];
+}
+
+- (void) testLoginConnectionWrongUrl {
+    
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Connection with wrong url"];
+    NSDictionary *params = @{@"login":@"robert_fdsr", @"password":@"fl5>[dWn"};
+    
+    [NetworkRequest POST:@"https://www.google.com" parameters:params blockCompletion:^(AFHTTPRequestOperation *operation, id responseObject) {
+        [expectation fulfill];
+        XCTFail(@"fail");
+    } andErrorCompletion:^(AFHTTPRequestOperation *operation, NSError *error) {
+        [expectation fulfill];
+        XCTAssert(true, @"Pass");
+    }];
+    
+    [self waitForExpectationsWithTimeout:5 handler:nil];
+}
+
+- (void) testLoginConnectionWrongWithNoParameters {
+    
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Connection with no parameters"];
+
+    [NetworkRequest POST:@"https://epitech-api.herokuapp.com/login" parameters:nil blockCompletion:^(AFHTTPRequestOperation *operation, id responseObject) {
+        [expectation fulfill];
+        XCTAssert(false, @"fail");
+    } andErrorCompletion:^(AFHTTPRequestOperation *operation, NSError *error) {
+        [expectation fulfill];
+        XCTAssert(true, @"Pass");
+    }];
+    
+    [self waitForExpectationsWithTimeout:5 handler:nil];
 }
 
 - (void)testExample {
---
+++ Epitech/Pods/Target Support Files/Pods-EpitechTests-AsyncDisplayKit/Pods-EpitechTests-AsyncDisplayKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EpitechTests_AsyncDisplayKit : NSObject
+@end
+@implementation PodsDummy_Pods_EpitechTests_AsyncDisplayKit
+@end
